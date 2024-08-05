56cd7c76e7b3f8eb034d0b311b871fcd04680f3d
HalcyonAlcedo <41666148+HalcyonAlcedo@users.noreply.github.com>
2024-08-05 20:19:31 +0800

    fix: 修复渲染器默认配置未生效问题

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,11 @@
 PORT=${KARIN_SUPPORT_PORT}
 # 配置文件路径
 CONFIG_FILE="/home/karin/config/config/server.yaml"
+DEFCONFIG_FILE="/home/karin/node_modules/node-karin/config/defSet/server.yaml"
 
 # 检查配置文件是否存在
-if [ -f "$CONFIG_FILE" ]; then
+if [ ! -f "$CONFIG_FILE" ]; then
+    cp "$DEFCONFIG_FILE" "$CONFIG_FILE"
     # 查找并替换 render: [] 为 render: [http://karin-support:PORT/puppeteer/]
     sed -i "s|render: \[\]|render:\n  - http://karin-support:${PORT}/puppeteer/|g" "$CONFIG_FILE"
 fi
